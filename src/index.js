149b70026926966a62efb4f9c3db75b1e4e54f0d
Kyeongmin Kim <kkfenix3085@naver.com>
2021-02-12 01:18:42 +0900

    Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
+import { createStore } from "redux";
+
 const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-number.innerText = count;
-
-const updateNumber = () => {
-    number.innerText = count;
+const countModifier = (count = 0) => {
+    return count;
 };
 
-const plusHandler = () => {
-    count = count + 1;
-    updateNumber();
-};
-
-const minusHandler = () => {
-    count = count - 1;
-    updateNumber();
-};
-
-plus.addEventListener("click", plusHandler);
-minus.addEventListener("click", minusHandler);
+const countStore = createStore(countModifier);
+console.log(countStore.getState());
