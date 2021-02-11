b53a41e8614a92eca63e41a0ec71a346c6c10d8e
Kyeongmin Kim <kkfenix3085@naver.com>
2021-02-12 01:26:48 +0900

    Apply Action at Reducer

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0) => {
+const countModifier = (count = 0, action) => {
+    if (action.type === "PLUS") {
+        return count + 1;
+    } else if (action.type === "MINUS") {
+        return count - 1;
+    }
     return count;
 };
 
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "PLUS" });
+console.log(countStore.getState());
+countStore.dispatch({ type: "PLUS" });
+console.log(countStore.getState());
+countStore.dispatch({ type: "PLUS" });
+console.log(countStore.getState());
+countStore.dispatch({ type: "PLUS" });
 console.log(countStore.getState());
